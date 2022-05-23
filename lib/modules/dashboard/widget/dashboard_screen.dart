7f02e61711d678d1c0c0e843bbcba293ff26eaed
Ajaodolapo1301 <dollypearl1301>
2022-05-23 18:51:43 +0100

    order pages redesign

--- lib/modules/dashboard/widget/dashboard_screen.dart
+++ lib/modules/dashboard/widget/dashboard_screen.dart
@@ -1,8 +1,12 @@
 import "package:flutter/material.dart";
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:responsive_grid_list/responsive_grid_list.dart';
 
+import '../../../shared/style/border_style.dart';
+import '../../../shared/style/font_style.dart';
 import '../../../shared/utils/asset_images.dart';
+import '../../../shared/utils/colors.dart';
 import '../../../shared/widgets/forms/input_text.dart';
 
 class DashboardScreen extends StatefulWidget {
@@ -33,9 +37,85 @@ class _DashboardScreenState extends State<DashboardScreen> {
               keyboardType: TextInputType.visiblePassword,
               onSaved: (val) {},
             ),
+            SizedBox(
+              height: 10.h,
+            ),
+            ResponsiveGridList(
+              horizontalGridSpacing:
+                  15.w, // Horizontal space between grid items
+              horizontalGridMargin: 5.w, // Horizontal space around the grid
+              verticalGridMargin: 15.h, // Vertical space around the grid
+              minItemWidth:
+                  300, // The minimum item width (can be smaller, if the layout constraints are smaller)
+              minItemsPerRow:
+                  2, // The minimum items to show in a single row. Takes precedence over minItemWidth
+              maxItemsPerRow:
+                  5, // The maximum items to show in a single row. Can be useful on large screens
+              shrinkWrap: true, // shrinkWrap property of the ListView.builder()
+              children: List.generate(DashboardModel.list.length, (index) {
+                return DashboardWidget(
+                  dashboardModel: DashboardModel.list[index],
+                );
+              }), // The list of widgets in the list
+            ),
           ],
         ),
       ),
     );
   }
+}
+
+class DashboardWidget extends StatelessWidget {
+  final VoidCallback? onTap;
+  final DashboardModel? dashboardModel;
+
+  const DashboardWidget({
+    Key? key,
+    this.dashboardModel,
+    this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        height: 177.h,
+        width: 159.w,
+        decoration: kDecoration.copyWith(color: Colors.white),
+        child: Column(children: [
+          Expanded(child: SvgPicture.asset(dashboardModel!.image!)),
+          Container(
+            height: 48.h,
+            width: 127.w,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5.r),
+                color: Color(0xffF8FDFF),
+                border: Border.all(color: AppColors.greyStroke)),
+            child: Center(
+                child: Text(
+              dashboardModel!.text!,
+              textAlign: TextAlign.center,
+              style: kBold400.copyWith(fontSize: 15.sp),
+            )),
+          ),
+          SizedBox(
+            height: 10.w,
+          )
+        ]),
+      ),
+    );
+  }
+}
+
+class DashboardModel {
+  String? text;
+  String? image;
+  DashboardModel({this.text, this.image});
+  static List<DashboardModel> list = [
+    DashboardModel(text: "Make Up", image: AssetResources.makeUp),
+    DashboardModel(text: "Website Development", image: AssetResources.driving),
+    DashboardModel(text: "Cleaning", image: AssetResources.cooking),
+    DashboardModel(text: "Mobile chef", image: AssetResources.cleaning),
+  ];
 }
--- lib/shared/utils/asset_images.dart
+++ lib/shared/utils/asset_images.dart
@@ -15,15 +15,19 @@ class AssetResources {
   // HOME unselected
   static const unSelectedAppointment =
       "assets/images/dashboard/appointment.svg";
-  static const unSelectedOrder = "assets/images/dashboard/order.svg";
-  static const unSelectedProfile = "assets/images/dashboard/profile.svg";
-  static const unselectedHome =
-      "assets/images/dashboard/dashboard-outlined.svg";
+  static const unSelectedOrder = "assets/images/home/order.svg";
+  static const unSelectedProfile = "assets/images/home/profile.svg";
+  static const unselectedHome = "assets/images/home/dashboard-outlined.svg";
 
-  static const selectedAppointment = "assets/images/dashboard/appointment.svg";
-  static const selectedOrder = "assets/images/dashboard/selectedOrder.svg";
-  static const selectedProfile = "assets/images/dashboard/selectedProfile.svg";
-  static const selectedHome = "assets/images/dashboard/dashboard-filled.svg";
+  static const selectedAppointment = "assets/images/home/appointment.svg";
+  static const selectedOrder = "assets/images/home/selectedOrder.svg";
+  static const selectedProfile = "assets/images/home/selectedProfile.svg";
+  static const selectedHome = "assets/images/home/dashboard-filled.svg";
+
+  static const makeUp = "assets/images/home/makeup.svg";
+  static const cleaning = "assets/images/home/cleaning.svg";
+  static const driving = "assets/images/home/driving.svg";
+  static const cooking = "assets/images/home/cooking.svg";
 
 //  Profile
 
@@ -47,7 +51,6 @@ class AssetResources {
   static const inflow = "assets/images/profile/inflow.svg";
   static const cardImage = "assets/images/profile/ovalBlue.png";
   static const calendar = "assets/images/profile/icons_calendar.svg";
-
   static const closedEye = "assets/images/profile/closed_eye.svg";
   static const openEye = "assets/images/profile/open_eye.svg";
   static const secure = "assets/images/profile/portfolio_secure.png";
